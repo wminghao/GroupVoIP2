875f8c568d100dfa563112c91419049e0371afb2
Ubuntu <ubuntu@ip-172-31-26-106.us-west-2.compute.internal>
2014-08-26 21:18:18 +0000

    added red5 project

--- server/mixcoder/test/runfourvideotest.bat
+++ server/mixcoder/test/runfourvideotest.bat
@@ -3,4 +3,6 @@ scons type=debug
 cd mixcoder/test
 rm abc*.flv
 g++ -o fourflvtest fourflvtest.cpp flvrealtimeparser.cpp
-./fourflvtest test.flv test2.flv test3.flv Flower.flv| ../../build/Linux-x86_64/mixcoder/prog/mix_coder | ../../build/Linux-x86_64/mixcoder/prog/seg_output_parser abc
+date
+./fourflvtest test.flv test2.flv test3.flv test4.flv| ../../build/Linux-x86_64/mixcoder/prog/mix_coder | ../../build/Linux-x86_64/mixcoder/prog/seg_output_parser abc
+date
