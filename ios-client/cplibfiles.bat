a3fd94dc9a7aa2357c20c467f462106dd5bc5ba8
Howard Wang <howard@Howards-MacBook-Air.local>
2014-10-03 11:00:30 -0700

    fix the path

--- ios-client/cplibfiles.bat
+++ ios-client/cplibfiles.bat
@@ -1,12 +1,12 @@
 mkdir vlclibs
 cd vlclibs
 mkdir Release-iphoneos
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphoneos/libMobileVLCKit.a Release-iphoneos
+cp -r ~/GroupVoIP.VLCKitIOS/build/Release-iphoneos/libMobileVLCKit.a Release-iphoneos
 mkdir Release-iphonesimulator
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphonesimulator/libMobileVLCKit.a Release-iphonesimulator
+cp -r ~/GroupVoIP.VLCKitIOS/build/Release-iphonesimulator/libMobileVLCKit.a Release-iphonesimulator
 mkdir Debug-iphoneos
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphoneos/libMobileVLCKit.a Debug-iphoneos
+cp -r ~/GroupVoIP.VLCKitIOS/build/Debug-iphoneos/libMobileVLCKit.a Debug-iphoneos
 mkdir Debug-iphonesimulator
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Debug-iphonesimulator/libMobileVLCKit.a Debug-iphonesimulator
-cp -r /Users/wminghao/Develop/vlc-iOS/VLCKit/build/Release-iphoneos/include .
+cp -r ~/GroupVoIP.VLCKitIOS/build/Debug-iphonesimulator/libMobileVLCKit.a Debug-iphonesimulator
+cp -r ~/GroupVoIP.VLCKitIOS/build/Release-iphoneos/include .
 cd ..
