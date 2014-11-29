89b31aac8ecdc6f97422f66907c9fe2ea7d400db
Howard Wang <howard@Howards-MacBook-Air.local>
2014-11-28 16:19:37 -0800

    added a missing file

---
+++ red5-readonly/src/main/java/org/red5/server/mixer/FLVArchiver.java
@@ -0,0 +1,68 @@
+package org.red5.server.mixer;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+import org.red5.logging.Red5LoggerFactory;
+import org.red5.server.api.Red5;
+import org.slf4j.Logger;
+
+public class FLVArchiver {
+	private String archivePrefix_;
+    private OutputStream outputFile_ = null;
+    private static Logger log = Red5LoggerFactory.getLogger(Red5.class);
+    
+	public FLVArchiver(String archivePath) {
+		archivePrefix_ = archivePath;		
+	}
+	public void startArchive() {
+		try {
+		    //log.info("=====>Writing binary file... outputFile={}", this.outputFilePath);
+		    if( outputFile_ == null ) {
+		    	String filePath = archivePrefix_+"archive.flv";
+            	outputFile_ = new BufferedOutputStream(new FileOutputStream(filePath));
+		    }
+		    if( outputFile_ != null ) {
+    			//13 bytes flv header
+    			outputFile_.write('F');
+    			outputFile_.write('L');
+    			outputFile_.write('V');
+    			outputFile_.write(0x1);
+    			outputFile_.write(0x5);
+    			outputFile_.write(0);
+    			outputFile_.write(0);
+    			outputFile_.write(0);
+    			outputFile_.write(0x9);
+    			outputFile_.write(0);
+    			outputFile_.write(0);
+    			outputFile_.write(0);
+    			outputFile_.write(0);
+		    }
+		}
+		catch(FileNotFoundException ex){
+		    log.info("======>Output flv File not found.");
+		}
+		catch(IOException ex){
+		    log.info("======>IO flv exception.");
+		}
+	}
+	public void stopArchive() {
+		try {
+	    	outputFile_.close();
+	    	outputFile_ = null;
+	    }catch (IOException ex) {
+	    	log.info("close flv exception:  {}", ex);
+    	}
+	}
+	public void archiveData(ByteBuffer frame, int startOffset, int flvFrameLen) {
+	    try {
+			outputFile_.write(frame.array(), startOffset, flvFrameLen-startOffset);
+		} catch (IOException e) {
+	    	log.info("flv write exception:  {}", e);
+		}
+	}
+}
