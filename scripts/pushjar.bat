6936e9fb200a5cd9e8faf9b7bcd872abf36ef8da
Howard <wminghao@yahoo.com>
2015-03-03 11:30:32 -0800

    use /proc/loadavg for now to calculate result.

---
+++ scripts/pushjar.bat
@@ -0,0 +1,2 @@
+scp -i ~/Google\ Drive/aws/visparcorp.pem ../red5-readonly/target/red5-server-1.0.2-RC4.jar ubuntu@52.10.61.227:
+scp -i ~/Google\ Drive/aws/visparcorp.pem ../red5-readonly/target/red5-server-1.0.2-RC4.jar ubuntu@54.148.16.2:
