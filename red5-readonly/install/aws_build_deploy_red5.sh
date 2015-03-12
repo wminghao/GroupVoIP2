51b988af52582c1b68129e1567d16206e066d729
root <root@vispar.com-ali>
2015-03-13 03:15:15 +0800

    make sure the videofiles are in a common location.

--- red5-readonly/install/aws_build_deploy_red5.sh
+++ red5-readonly/install/aws_build_deploy_red5.sh
@@ -3,8 +3,4 @@ cd ..
 cd target
 tar xzvf red5-server-1.0.2-RC4-server.tar.gz
 sudo cp -r red5-server-1.0.2-RC4 /usr/share/
-cd ../external
-sudo cp -r lib/ /usr/share/red5-server-1.0.2-RC4/
-sudo cp -r red5* /usr/share/red5-server-1.0.2-RC4/conf/
-sudo cp -r VisparApp  /usr/share/red5-server-1.0.2-RC4/webapp/
 cd ../install
--- red5-readonly/install/aws_install_step3.sh
+++ red5-readonly/install/aws_install_step3.sh
@@ -1,7 +1,11 @@
 sudo apt-get install curl
 sudo apt-get install wget
 #./aws_build_deploy_red5.sh
-cp -r ../external/videofiles/ ~/
+cd ../external
+sudo cp -r lib/ /usr/share/red5-server-1.0.2-RC4/
+sudo cp -r red5* /usr/share/red5-server-1.0.2-RC4/conf/
+sudo cp -r VisparApp  /usr/share/red5-server-1.0.2-RC4/webapp/
+sudo cp -r videofiles /usr/share/red5-server-1.0.2-RC4/
 cd ..
 ./jni.bat
 sudo cp red5 /etc/init.d/red5
