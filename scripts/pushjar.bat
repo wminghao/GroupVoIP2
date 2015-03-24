39adc894fee2d2a7f9691392cee02ba81e88b4e7
Howard <wminghao@yahoo.com>
2015-03-24 14:51:19 -0700

    use ip address to avoid dns lookup error from Aliyun server

--- scripts/pushjar.bat
+++ scripts/pushjar.bat
@@ -2,3 +2,4 @@ scp -i ~/Google\ Drive/aws/visparcorp.pem ../VisparApp/build/classes/org/red5/co
 scp -i ~/Google\ Drive/aws/visparcorp.pem ../VisparApp/build/classes/org/red5/core/*.class ubuntu@54.148.16.2:
 scp -i ~/Google\ Drive/aws/visparcorp.pem ../red5-readonly/target/red5-server-1.0.2-RC4.jar ubuntu@52.10.61.227:
 scp -i ~/Google\ Drive/aws/visparcorp.pem ../red5-readonly/target/red5-server-1.0.2-RC4.jar ubuntu@54.148.16.2:
+#scp ../red5-readonly/target/red5-server-1.0.2-RC4.jar root@121.40.163.69
